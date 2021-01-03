fa27ca2e07828df86e2aa564d9ac15de5d253d3b
Marc Chisinevski <marcgcp6@gmail.com>
2021-01-03 15:56:15 -0800

    Update getsatoken.sh

--- getsatoken.sh
+++ getsatoken.sh
@@ -1,2 +1,6 @@
-kubectl get secret $(kubectl get sa mysa -n default -o json | jq -r '.secrets[0].name') -o json | jq -r '.data.token' | base64 -d
+kubectl get secret $(kubectl get sa default -n default -o json | jq -r '.secrets[0].name') -o json | jq -r '.data.token' | base64 -d
+export KUBE_AZ=$(kubectl get secret $(kubectl get sa default -n default -o json | jq -r '.secrets[0].name') -o json | jq -r '.data.token' | base64 -d) 
+curl  -H "Authorization: Bearer $KUBE_AZ" --insecure https://0.0.0.0:32768/api
+
+
 
